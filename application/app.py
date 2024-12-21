1b0b4dd3cbe95c074b2856b17a15caed3859817a
Fabrizio Membrino <membrfab@students.zhaw.ch>
2024-12-21 19:08:18 +0100

    remove debug

--- application/app.py
+++ application/app.py
@@ -47,4 +47,4 @@ def home():
 # Flask-Server starten
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
